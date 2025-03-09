1c8860dcca2c82ca8598fa6a86e480c8b87e1e5b
fqnet <fqnet@dhcp-131-225-169-166.dhcp.fnal.gov>
2025-03-09 18:03:48 -0500

    add script to control scope to take data

---
+++ acquisition.py
@@ -0,0 +1,258 @@
+# adapted for MSO-X 92004A
+
+"""
+VISA Control: FastFrame Acquisition
+Tektronix DPO7254 Control
+FNAL November 2018
+CMS MTD ETL Test beam
+"""
+
+import numpy as np
+# import matplotlib.pyplot as plt
+import sys
+import optparse
+import argparse
+import signal
+import os
+import time
+import shutil
+import datetime
+from shutil import copy
+ 
+stop_asap = False
+
+import pyvisa 
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        shutil.copytree(item, d, symlinks, ignore)
+def copynew(source,destination):
+    for files in source:
+        shutil.copy(files,destination)
+
+"""#################SEARCH/CONNECT#################"""
+# establish communication with dpo
+rm = pyvisa.ResourceManager("@py")
+print(rm.list_resources())
+dpo = rm.open_resource('USB0::10893::36867::MY64060141::0::INSTR')
+
+dpo.timeout = 3000000
+dpo.encoding = 'latin_1'
+print(dpo.query('*idn?'))
+parser = argparse.ArgumentParser(description='Run info.')
+
+parser.add_argument('--numEvents',metavar='Events', type=str,default = 500, help='numEvents (default 500)',required=True)
+parser.add_argument('--runNumber',metavar='runNumber', type=str,default = -1, help='runNumber (default -1)',required=False)
+parser.add_argument('--laserCH',metavar='laserCH', type=str,default = 1, help='laser channel (default 1)',required=True)
+parser.add_argument('--snspdCH',metavar='snspdCH', type=str,default = 2, help='snspd channel (default 2)',required=True)
+parser.add_argument('--sampleRate',metavar='sampleRate', type=str,default = 20, help='Sampling rate (default 20)',required=True)
+parser.add_argument('--horizontalWindow',metavar='horizontalWindow', type=str,default = 125, help='horizontal Window (default 125)',required=True)
+## parser.add_argument('--numPoints',metavar='Points', type=str,default = 500, help='numPoints (default 500)',required=True)
+parser.add_argument('--vScale1',metavar='vScale1', type=float, default= 0.02, help='Vertical scale, volts/div',required=False)
+parser.add_argument('--vScale2',metavar='vScale2', type=float, default= 0.02, help='Vertical scale, volts/div',required=False)
+parser.add_argument('--vScale3',metavar='vScale3', type=float, default= 0.02, help='Vertical scale, volts/div',required=False)
+parser.add_argument('--vScale4',metavar='vScale4', type=float, default= 0.02, help='Vertical scale, volts/div',required=False)
+#
+parser.add_argument('--vPos1',metavar='vPos1', type=float, default= 0.02, help='Vertical offset, div',required=False)
+parser.add_argument('--vPos2',metavar='vPos2', type=float, default= 0.02, help='Vertical offset, div',required=False)
+parser.add_argument('--vPos3',metavar='vPos3', type=float, default= 0.02, help='Vertical offset, div',required=False)
+parser.add_argument('--vPos4',metavar='vPos4', type=float, default= 0.02, help='Vertical offset, div',required=False)
+parser.add_argument('--timeoffset',metavar='timeoffset', type=float, default=-130, help='Offset to compensate for trigger delay. This is the delta T between the center of the acquisition window and the trigger. (default for NimPlusX: -160 ns)',required=False)
+
+parser.add_argument('--save',metavar='save', type=int, default= 1, help='Save waveforms',required=False)
+parser.add_argument('--timeout',metavar='timeout', type=float, default= -1, help='Max run duration [s]',required=False)
+
+
+args = parser.parse_args()
+runNumberParam = str(args.runNumber) 
+laserCH = args.laserCH
+snspdCH = args.snspdCH
+date = datetime.datetime.now()
+savewaves = int(args.save)
+timeout = float(args.timeout)
+numEvents = int(args.numEvents) # number of events for each file
+# numPoints = int(args.numPoints) # number of points to be acquired per event
+print(savewaves)
+print("timeout is ",timeout)
+"""#################CONFIGURE INSTRUMENT#################"""
+date = datetime.datetime.now()
+hScale = float(args.horizontalWindow)*1e-9
+timeoffset = float(args.timeoffset)*1e-9
+samplingrate = float(args.sampleRate)*1e+9
+numPoints = samplingrate*hScale
+
+vScale_ch1 =float(args.vScale1) # in Volts for division
+vScale_ch2 =float(args.vScale2) # in Volts for division
+vScale_ch3 =float(args.vScale3) # in Volts for division
+vScale_ch4 =float(args.vScale4) # in Volts for division
+
+#vertical position
+vPos_ch1 = float(args.vPos1)
+vPos_ch2 = float(args.vPos2)
+vPos_ch3 = float(args.vPos3)
+vPos_ch4 = float(args.vPos4)
+
+"""#################CONFIGURE RUN NUMBER#################"""
+# increment the last runNumber by 1
+
+if runNumberParam == -1:
+	RunNumberFile = '/home/fqnet/Desktop/ScopeHandler/KeySight/Acquisition/otsdaq_runNumber.txt'
+	with open(RunNumberFile) as file:
+	    runNumber = int(file.read())
+	print('######## Starting RUN {} ########\n'.format(runNumber))
+	print('---------------------\n')
+	print(date)
+	print('---------------------\n')
+	
+	with open(RunNumberFile,'w') as f:
+	    f.write(str(runNumber+1))
+
+else: runNumber = runNumberParam
+
+"""#################SET THE OUTPUT FOLDER#################"""
+# The scope save runs localy on a shared folder with
+
+path = r"C:\Users\Public\Documents\Infiniium\Waveforms/ADR_data/" + datetime.date.today().strftime("%Y%m%d") + "_0p8K/"
+dpo.write(':DISK:MDIRectory "{}"'.format(path)) ## mkdir 
+log_path = "/home/fqnet/Desktop/ScopeHandler/KeySight/Acquisition/Logbook.txt"
+run_log_path = "/home/fqnet/Desktop/ScopeHandler/KeySight/Acquisition/RunLog.txt"
+
+#Write in the log file
+logf = open(log_path,"a+")
+logf.write("\n\n#### SCOPE LOGBOOK -- RUN NUMBER {} ####\n\n".format(runNumber))
+logf.write("Date:\t{}\n".format(date))
+logf.write("---------------------------------------------------------\n")
+logf.write("Number of events per file: {} \n".format(numEvents))
+logf.write("---------------------------------------------------------\n\n")
+
+
+"""#################SCOPE HORIZONTAL SETUP#################"""
+# dpo setup
+
+dpo.write(':STOP;*OPC?')
+
+dpo.write(':TIMebase:RANGe {}'.format(hScale)) ## Sets the full-scale horizontal time in s. Range value is ten times the time-per division value.
+dpo.write(':TIMebase:REFerence:PERCent 50') ## percent of screen location
+dpo.write(':ACQuire:SRATe:ANALog {}'.format(samplingrate))
+dpo.write(':TIMebase:POSition {}'.format(timeoffset)) ## offset
+dpo.write(':ACQuire:MODE SEGMented') ## fast frame/segmented acquisition mode
+dpo.write(':ACQuire:SEGMented:COUNt {}'.format(numEvents)) ##number of segments to acquire
+#dpo.write(':ACQuire:POINts:ANALog {}'.format(numPoints))
+dpo.write(':ACQuire:INTerpolate 0') ## interpolation is set off (otherwise its set to auto, which cause errors downstream)
+
+"""#################SCOPE CHANNELS BANDWIDTH#################"""
+# dpo.write(':ACQuire:BANDwidth MAX') ## set the bandwidth to maximum
+# dpo.write('CHANnel1:ISIM:BANDwidth 2.0E+09')
+# dpo.write('CHANnel2:ISIM:BANDwidth 2.0E+09')
+# dpo.write('CHANnel3:ISIM:BANDwidth 2.0E+09')
+# dpo.write('CHANnel4:ISIM:BANDwidth 2.0E+09')
+
+# dpo.write('CHANnel1:ISIM:BWLimit 1')
+# dpo.write('CHANnel2:ISIM:BWLimit 1')
+# dpo.write('CHANnel3:ISIM:BWLimit 1')
+# dpo.write('CHANnel4:ISIM:BWLimit 1')
+
+#dpo.write(':ACQuire:BANDwidth 2.E9')
+"""#################SCOPE VERTICAL SETUP#################"""
+#vScale expressed in Volts
+dpo.write('CHANnel1:SCALe {}'.format(vScale_ch1))
+dpo.write('CHANnel2:SCALe {}'.format(vScale_ch2))
+dpo.write('CHANnel3:SCALe {}'.format(vScale_ch3))
+dpo.write('CHANnel4:SCALe {}'.format(vScale_ch4))
+
+dpo.write('CHANnel1:OFFSet {}'.format(-vScale_ch1 * vPos_ch1))
+dpo.write('CHANnel2:OFFSet {}'.format(-vScale_ch2 * vPos_ch2))
+dpo.write('CHANnel3:OFFSet {}'.format(-vScale_ch3 * vPos_ch3))
+dpo.write('CHANnel4:OFFSet {}'.format(-vScale_ch4 * vPos_ch4))
+
+
+logf.write("VERTICAL SETUP\n")
+logf.write('- CH1: vertical scale set to {} V for division\n'.format(vScale_ch1))
+logf.write('- CH2: vertical scale set to {} V for division\n'.format(vScale_ch2))
+logf.write('- CH3: vertical scale set to {} V for division\n'.format(vScale_ch3))
+logf.write('- CH4: vertical scale set to {} V for division\n\n'.format(vScale_ch4))
+
+
+"""#################TRIGGER SETUP#################"""
+dpo.write("TRIGger:MODE SEQuence")
+dpo.write(f"TRIGger:LEVel CHANnel{laserCH},0.5")
+dpo.write(f"TRIGger:LEVel CHANnel{snspdCH},-0.06")
+dpo.write(":TRIGger:SEQuence:TERM1 EDGE1")
+dpo.write(":TRIGger:SEQuence:TERM2 EDGE2")
+dpo.write("TRIGger:SEQuence:RESet:ENABle 1")
+dpo.write("TRIGger:SEQuence:RESet:TYPE TIME")
+dpo.write("TRIGger:SEQuence:RESet:TIME 1e-7")
+dpo.write(f"TRIGger:EDGE1:SOURce CHAN{laserCH}")
+dpo.write("TRIGger:EDGE1:SLOPe POSitive")
+dpo.write(f"TRIGger:EDGE2:SOURce CHAN{snspdCH}")
+dpo.write("TRIGger:EDGE2:SLOPe NEGative")
+print('Horizontal, vertical, and trigger settings configured.\n')
+
+status = ""
+status = "busy"
+
+run_logf = open(run_log_path,"w")
+run_logf.write(status)
+run_logf.close()
+
+"""#################DATA TRANSFERRING#################"""
+# configure data transfer settings
+time.sleep(2)
+
+print(dpo.write(':CDISplay'))
+
+dpo.write('*CLS;:SINGle')
+start = time.time()
+end_early = False
+while True:
+#	print "Ader is ",dpo.query(':ADER?')
+#	print "OPC is ",dpo.query('*OPC?')
+	if (int(dpo.query(':ADER?')) == 1): 
+		print("Acquisition complete")
+		break
+	else:
+		#print "Still waiting" 
+		time.sleep(0.1)
+		if not savewaves and timeout > 0 and time.time() - start > timeout:
+			end_early=True
+			dpo.write(':STOP;*OPC?')
+			break
+
+end = time.time()
+#print(dpo.query('*OPC?'))
+# print("Trigger!")
+
+tmp_file = open(run_log_path,"w")
+status = "writing"
+tmp_file.write(status)
+tmp_file.write("\n")
+tmp_file.close()
+
+duration = end - start
+trigRate = float(numEvents)/duration
+if not end_early: print("\nRun duration: %0.2f s. Trigger rate: %.2f Hz\n" % (duration,trigRate))
+else: print("\nRun duration: %0.2f s. Trigger rate: unknown\n" % (duration) )
+if savewaves: 
+	dpo.write(':DISK:SEGMented ALL') ##save all segments (as opposed to just the current segment)
+	print(dpo.query('*OPC?'))
+	print("Ready to save all segments")
+	time.sleep(0.5)
+	for i in [int(laserCH),int(snspdCH)]:
+		dpo.write(f':DISK:SAVE:WAVeform CHANnel{i} ,"{path}\\Wavenewscope_CH{i}_{runNumber}",H5,ON')
+
+		print(dpo.query('*OPC?'))
+		print(f"Saved Channel {i} waveform")
+		time.sleep(1)
+else: print("Skipping saving step.")
+tmp_file2 = open(run_log_path,"w")
+status = "ready"
+tmp_file2.write(status)
+tmp_file2.write("\n")
+tmp_file2.close()
+
+
+dpo.close()
+
+
---
+++ acquisition_wrapper.py
@@ -0,0 +1,121 @@
+'''
+Code to take PCR and DCR with n number of channels
+records count per second for each channel and coincidences between each channel
+Christina Wang 11/27/2024
+
+'''
+
+import itertools
+import sys
+from time import sleep
+import TimeTagger
+import datetime
+import pyvisa as visa
+import numpy as np
+import csv
+import os
+from TimeTagger import Coincidences, Coincidences, CoincidenceTimestamp
+sys.path.append("lib")
+
+def get_voltage(ch):
+	write("CONN {},'quit'".format(ch))
+	try:
+		return float(query("VOLT?"))
+	except:
+		print("error getting voltage")
+	finally:
+		write("quit")
+def set_voltage(ch, volt):
+	write("CONN {},'quit'".format(ch))
+	write('VOLT {}'.format(volt))
+	write("quit")
+def write(msg):sim.write(msg)
+def query(msg):
+	write(msg)
+	return sim.read()
+def turnOn(ch):
+	write("CONN {}, 'quit'".format(ch))
+	write('OPON')
+	write('quit')
+def turnOff(ch):
+	write("CONN {}, 'quit'".format(ch))
+	if query('EXON?'):write('OPOF')
+	write('quit')
+
+########################################################################
+### define bias and readout channels, threshold, and coincidence window
+########################################################################
+biases = [4,1,2]
+pixels = [1,2,3]
+scope_ch = [2,3,4]
+nevents = 10000
+tries = 5
+
+##### scope settings
+vScale_ch1 = 0.5
+vScale_ch2 = 0.05
+vScale_ch3 = 0.05
+vScale_ch4 = 0.05
+vPos_ch1 = -1.3
+vPos_ch2 = 2
+vPos_ch3 = 2
+vPos_ch4 = 2
+hScale = 200 #ns
+hOffset = 20 #ns
+sampleRate = 128 #GSa/s
+##########################################	
+### define voltages and data taking time in the channels
+##########################################
+voltages = np.round([0.140,0.145,0.150,0.155,0.160,0.170,0.180,0.190,0.200,0.210,0.220, 0.23],3)
+print("###################################################")
+print("####### Voltages to scan #######")
+print("###################################################")
+print(voltages)
+print(" ")
+
+
+if os.getenv("USER") == "fqnet": cryo = "ADR"
+elif os.getenv("USER") == "fcctt": cryo = "spot1"
+else: exit("CAN NOT FIND USER")
+print(" ")
+print(f"cryostat used is {cryo}")
+print(" ")
+
+if __name__ == "__main__":
+	###########################################
+	# establish connection to voltage source
+	##########################################
+	rm=visa.ResourceManager()
+	sim = rm.open_resource('ASRL/dev/ttyUSB0::INSTR')
+	print("Main frame: ", query("*IDN?"))
+	for ch_i, bias_ch in enumerate(biases):
+		name = f"B{bias_ch}_P{pixels[ch_i]}"
+		sim.write("CONN {}, 'quit'".format(bias_ch))
+		print(f"Slot {bias_ch}:", query("*idn?"))
+		set_voltage(bias_ch, 0.0)
+		for j, v in enumerate(voltages):
+        	        # set bias voltages
+        	        print(v, datetime.datetime.now())
+        	        set_voltage(bias_ch, voltages[j])
+        	        tries_i = 0
+        	        get_v = get_voltage(bias_ch)
+        	        while tries_i < tries and (get_v is None or abs(voltages[j]-get_v)>=0.0001):
+        	                set_voltage(bias_ch, voltages[j])
+        	                get_v = get_voltage(bias_ch)
+        	                print("error", tries_i, get_v, voltages[j])
+        	                tries_i += 1
+        	        if get_v is None or abs(voltages[j]-get_v)>=0.0001:
+        	                print("channel", bias_ch, voltages[j], get_v)
+        	                sys.exit("VOLTAGE IS OFF FROM TARGET")
+        	        sleep(0.5)
+        	        turnOn(bias_ch)
+        	        # data taking with timetagger
+        	        print(f"start data acquisition for BV{voltages[j]}")
+        	        bv_string = str(voltages[j]).replace(".","p") 
+        	        cmd = f"sudo python3 acquisition.py --numEvents {nevents} --runNumber {name}_BV{bv_string} --laserCH 1 --snspdCH {scope_ch[ch_i]}\
+ --horizontalWindow {hScale} --timeoffset {hOffset} --sampleRate {sampleRate} \
+--vScale1 {vScale_ch1} --vScale2 {vScale_ch2} --vScale3 {vScale_ch3} --vScale4 {vScale_ch4} \
+--vPos1 {vPos_ch1} --vPos2 {vPos_ch2} --vPos3 {vPos_ch3} --vPos4 {vPos_ch4}"
+        	        print(cmd)
+        	        os.system(cmd)
+        	        turnOff(bias_ch)
